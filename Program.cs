a9b8a13256923f24163a7b015fde779111023c24
raj1020 <cas021911@coderacademy.edu.au>
2020-04-07 13:32:08 +1000

    started a new C# program for guessing passwords

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Password_retry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctPassword = "BigfootWasHere!";
+            
+
+            for (int attemptsRemaining=3; attemptsRemaining >0; attemptsRemaining-- )
+            {
+                Console.WriteLine($"Please enter the correct password. You can try {attemptsRemaining} times." );
+                string password = Console.ReadLine();
+                if (password == correctPassword) {
+                    Console.WriteLine("Congratulations. You entered the right password");
+                    break;
+                }
+
+                if (attemptsRemaining <= 1)
+                {
+                    Console.WriteLine("You password did not match You have already entered 3 incorrect passwords.");
+                }
+                else
+                {
+                    Console.WriteLine("Your password did not match");
+                }
+                    
+                    
+                  
+                
+                
+
+               
+
+            }
+
+           
+
+            }
+
+        }
+    }
+
